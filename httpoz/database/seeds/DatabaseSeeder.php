92af9b30944dfb1358e8282d24870e2099d80e75
catupe <mesa.alvaro@gmail.com>
2017-11-27 12:14:45 -0300

    arreglado

--- httpoz/database/seeds/DatabaseSeeder.php
+++ httpoz/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         $this->call(UserRolesSeeder::class);
-        $this->call(Balanza1TableSeeder::class);
+        $this->call(BalanzaTableSeeder::class);
     }
 }
