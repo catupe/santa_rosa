73a94f92e1c16167d37a17845ab0a6bcf71b7f50
catupe <mesa.alvaro@gmail.com>
2018-01-16 11:52:15 -0300

    cambio en la carga del csv

--- httpoz/app/Console/Commands/Balanza.php
+++ httpoz/app/Console/Commands/Balanza.php
@@ -61,35 +61,20 @@ class Balanza extends Command
               $total = $reader->get()->count();
 
               $header = $reader->all()->first()->keys()->toArray();
-              //var_dump($header);
-              //exit;
+
               // selecciono la balanza corresponduente a la corrida
               $balanza = \App\Balanza::where('id', '=', 1)->first();
               $idBalanza = $balanza->id;
-              /*+++
-              //// cargo los ids de las balanzas
-              $arr_ids_balanzas = array();
-              foreach ($header as $key => $value) {
-                  $balanza = \App\Balanza::where('nombre', '=', $value)->first();
-                  if(isset($balanza) and ($balanza != null)) {
-                      $arr_ids_balanzas[$value] = $balanza->id;
-                  }
-              }
-              +++*/
+
               $cant_leer = $total - $ultima_actualizada;
-              //var_dump($cant_leer);
               $reader->skipRows($ultima_actualizada);
               $datos = $reader->toArray();
 
-              //$fila = $ultima_actualizada;
-
-              // normalizo el csv para tener [fila][balanza][lectura]
-              //                             [fila][balanza][fecha]
-              $csv_normalizado = array();
+              $fila         = $ultima_actualizada;
+              $commit_cont  = 0;
               foreach ($datos as $key => $value) {
-                if(preg_match('/\d+\.?\d+/', $value[$k])) {
-                  //$csv_normalizado[$key][$idBalanza]["lectura"] = $value[$k];
-                  //$csv_normalizado[$key][$idBalanza]["date"]    = $value["date"] . ' ' . $value["time"];
+                  $fila++;
+
                   $balanza_lectura                    = new \App\BalanzaLectura;
                   $balanza_lectura->lectura           = trim($value[$header[0]]);//trim($datos["lectura"]);
                   $balanza_lectura->lectura_acumulada = trim($value[$header[1]]);
@@ -97,59 +82,20 @@ class Balanza extends Command
                   $balanza_lectura->comentarios       = "";
                   $balanza_lectura->created_at        = $value[$header[3]] . ' ' . $value[$header[4]];
                   $balanza_lectura->balanza_id        = $idBalanza;//$arr_ids_balanzas[$nombre_balanza];
-                  $balanza_lectura->fila              = $fila + 1 + $ultima_actualizada;
+                  $balanza_lectura->fila              = $fila;
                   $balanza_lectura->save();
                   $cant_registros++;
 
                   $commit_cont++;
-                  //error_log("cont => ".$commit_cont);
                   if( $commit_cont == 500 ) {
                     print("comiteo => ".$commit_cont."\r\n");
                     $this->info("commit : ".$commit_cont . '\n\r');
                     $commit_cont = 0;
                     DB::commit();
                   }
-                }
-              }
-              /*++++
-              foreach ($datos as $key => $value) {
-                  foreach ($arr_ids_balanzas as $k => $v) {
-                      $nombre_balanza = "";
-                      if(preg_match('/\d+\.?\d+/', $value[$k])) {
-                        $csv_normalizado[$key][$k]["lectura"] = $value[$k];
-                        $csv_normalizado[$key][$k]["date"] = $value["date"] . ' ' . $value["time"];
-                      }
-                  }
-              }
-              +++*/
-              //var_dump($csv_normalizado);
-              /*
-              $commit_cont = 0;
-              DB::beginTransaction();
-              foreach ($csv_normalizado as $fila => $value) {
-                  foreach ($value as $nombre_balanza => $datos) {
-                      //var_dump($datos);
-                      $balanza_lectura              = new \App\BalanzaLectura;
-                      $balanza_lectura->lectura     = trim($datos["lectura"]);
-                      $balanza_lectura->comentarios = "";
-                      $balanza_lectura->created_at  = $datos["date"];
-                      $balanza_lectura->balanza_id  = $arr_ids_balanzas[$nombre_balanza];
-                      $balanza_lectura->fila        = $fila + 1 + $ultima_actualizada;
-                      $balanza_lectura->save();
-                      $cant_registros++;
-
-                      $commit_cont++;
-                      //error_log("cont => ".$commit_cont);
-                      if( $commit_cont == 500 ) {
-                        print("comiteo => ".$commit_cont."\r\n");
-                        $this->info("commit : ".$commit_cont . '\n\r');
-                        $commit_cont = 0;
-                        DB::commit();
-                      }
-                  }
               }
               DB::commit();
-              */
+
           });
 
           $now = new \DateTime();
